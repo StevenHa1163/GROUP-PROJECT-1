84b699cf66399b3d1064deead7b0ea99ec8b0c9f
MBrandt <vashathearchivist6789@gmail.com>
2022-08-02 19:22:05 -0400

    polishing some functions. connecting HTMLs

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # GROUP-PROJECT-1
-GROUP PROJECT CREATING A WEBSITE THAT GATHERS AND DISPLAYS TV SHOWS AND RELEVANT INFORMATION
+This is a group project that allows the user to search for shows available on Television, Steaming, and Online Rental.
 
 # LINK
 https://stevenha1163.github.io/GROUP-PROJECT-1/
 
 # SCREENSHOT
-<!-- ![alt text](screenshot) -->
+<!-- ![alt text](screenshot link) -->
--- assets/js/script.js
+++ assets/js/script.js
@@ -15,17 +15,26 @@ var searchShow = document.querySelector("#searchBtn");
 var show = document.querySelector("#enterShow");
 
 searchShow.addEventListener("click", showSearch);
-
 // search function
 
-function showSearch() {
-  var input = show.value
-  var url = `https://imdb-api.com/en/API/MostPopularTVs/k_72kh8az4`
-  // var url = `http://api.openweathermap.org/go/1.0/direct?q=$e{input}&limit=1&appid=ac75e314d25573644ae4d9a903da5c8c`
-  fetch(url).then(function (res) {
-    return res.json();
-  }).then(function (data) {
-    console.log(data);
-  })
-};
+
+function handleSearchBoxSubmit(event) {
+  event.preventDefault();
+  var searchInputVal = document.querySelector("#enterShow");
+
+
+}
+
+
+
+// function showSearch() {
+//   var input = show.value
+//   var url = `https://imdb-api.com/en/API/MostPopularTVs/k_72kh8az4`
+//   // var url = `http://api.openweathermap.org/go/1.0/direct?q=$e{input}&limit=1&appid=ac75e314d25573644ae4d9a903da5c8c`
+//   fetch(url).then(function (res) {
+//     return res.json();
+//   }).then(function (data) {
+//     console.log(data);
+//   })
+// };
 
