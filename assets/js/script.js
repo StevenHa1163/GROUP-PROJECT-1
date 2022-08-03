c6c5099aae3cab793cd02c7afc6145343956f797
MBrandt <vashathearchivist6789@gmail.com>
2022-08-02 21:27:04 -0400

    finish working model

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,7 @@ const DISPLAY_LIMIT = 5;
 function showSearch() {
   var input = show.value
   var url = `https://imdb-api.com/en/API/MostPopularTVs/k_72kh8az4`
+  // var url = `https://imdb-api.com/en/API/SearchSeries/k_72kh8az4`
   // var url = `http://api.openweathermap.org/go/1.0/direct?q=$e{input}&limit=1&appid=ac75e314d25573644ae4d9a903da5c8c`
   fetch(url).then(function (res) {
     return res.json();
@@ -69,4 +70,11 @@ function showSearch() {
 //     console.log(data);
 //   })
 // };
+
+
+// function getParams() {
+//   var query = searchParamsArr = document.location.search.split('&')
+
+
+// }
 
