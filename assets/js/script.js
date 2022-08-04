d499bae0ef67e3e9795f146837adb533c4b254c6
Test1 1 <williamh2461@gmail.com>
2022-08-04 17:23:39 -0400

    made some changes to the showSearch function to make the search bar search by show title

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,9 @@
 var searchShow = document.querySelector("#searchBtn");
 var show = document.querySelector("#enterShow");
 
+// testing search input to search for title
+// var searchInputVal = document.querySelector("#search-input").value;
+// var queryString = ''
 // click event
 searchShow.addEventListener("click", showSearch);
 // search function
@@ -30,29 +33,72 @@ searchShow.addEventListener("click", showSearch);
 
 
 // full title rank
+// const DISPLAY_LIMIT = 5;
+// function showSearch() {
+//   var input = show.value
+//   var url = `https://imdb-api.com/en/API/MostPopularTVs/k_72kh8az4`
+//   // var url = 'https://api.watchmode.com/v1/title/345534/details/?apiKey=ci6ux2nzkeIC5BKgFcV6wO4d23T401iYpZuy7Won&append_to_response=sources'
+//   // var url = `https://imdb-api.com/en/API/SearchSeries/k_72kh8az4`
+//   // var url = `http://api.openweathermap.org/go/1.0/direct?q=$e{input}&limit=1&appid=ac75e314d25573644ae4d9a903da5c8c`
+//   fetch(url).then(function (res) {
+//     return res.json();
+//   }).then(function (data) {
+//     console.log(data)
+//     var resultsContainer = document.querySelector("#results");
+//     for (let index = 0; index < DISPLAY_LIMIT; index++) {
+//       const element = data.items[index];
+//       const itemContainer = document.createElement("div");
+//       const itemTitle = document.createElement("h2");
+//       const itemRank = document.createElement("p");
+//       itemTitle.textContent = element.fullTitle;
+//       itemRank.textContent = element.rank;
+//       itemContainer.append(itemTitle,itemRank);
+//       resultsContainer.append(itemContainer);
+//       console.log(resultsContainer);
+
+//     }
+//   })
+// };
+
+
+
+//TESTING
+
 const DISPLAY_LIMIT = 5;
 function showSearch() {
   var input = show.value
-  var url = `https://imdb-api.com/en/API/MostPopularTVs/k_72kh8az4`
-  // var url = `https://imdb-api.com/en/API/SearchSeries/k_72kh8az4`
+  // var url = `https://imdb-api.com/en/API/MostPopularTVs/k_72kh8az4`
+  // var url = 'https://api.watchmode.com/v1/title/345534/details/?apiKey=ci6ux2nzkeIC5BKgFcV6wO4d23T401iYpZuy7Won&append_to_response=sources'
+  var url = `https://imdb-api.com/en/API/SearchSeries/k_72kh8az4`
   // var url = `http://api.openweathermap.org/go/1.0/direct?q=$e{input}&limit=1&appid=ac75e314d25573644ae4d9a903da5c8c`
+  url = url + "/" + input
   fetch(url).then(function (res) {
     return res.json();
   }).then(function (data) {
+    console.log(data)
     var resultsContainer = document.querySelector("#results");
     for (let index = 0; index < DISPLAY_LIMIT; index++) {
-
-      const element = data.items[index];
+      const element = data.results[index];
       const itemContainer = document.createElement("div");
       const itemTitle = document.createElement("h2");
       const itemRank = document.createElement("p");
-      itemTitle.textContent = element.fullTitle;
-      itemRank.textContent = element.rank;
+      itemTitle.textContent = element.title;
+      // itemRank.textContent = element.rank;
       itemContainer.append(itemTitle,itemRank);
       resultsContainer.append(itemContainer);
+      console.log(resultsContainer);
+
     }
   })
 };
+
+//TESTING
+
+
+
+
+
+
 
 //trying to make the search function work and search certain parameters
 // function searchApi(query) {
